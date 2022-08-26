ad0cdb492fc1f9e807eaddc0ea91e8beeb63e16e
Sam5196 <sammedpachore1008@gmail.com>
2022-08-26 20:50:51 +0530

    Some arthimatic Operatins

---
+++ ArithmeticOperation.js
@@ -0,0 +1,11 @@
+let a = 2;
+let b = 8;
+let c = 6;
+let x = a + b * c;
+let y = c + a / b;
+let z = a % b + c;
+let m = a * b + c;
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(m);
