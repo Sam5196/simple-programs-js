e6a5b2b1ee05542681e3ee26f0e87ee925a39744
Sam5196 <sammedpachore1008@gmail.com>
2022-08-26 20:19:46 +0530

    Read the digit and print the word of the digit

---
+++ ReadDigit.js
@@ -0,0 +1,44 @@
+function digitValue(digit) {
+    switch (digit) {
+        case "0":
+            console.log("Zero");
+            break;
+        case "1":
+            console.log("One");
+            break;
+        case "2":
+            console.log("Two");
+            break;
+        case "3":
+            console.log("Three");
+            break;
+        case "4":
+            console.log("Four");
+            break;
+        case "5":
+            console.log("Five");
+            break;
+        case "6":
+            console.log("Six");
+            break;
+        case "7":
+            console.log("Seven");
+            break;
+        case "8":
+            console.log("Eight");
+            break;
+        case "9":
+            console.log("Nine");
+            break;
+        default:
+            console.log("Enter the correct number")
+    }
+}
+// It will print multiple Time at a time 
+function printNumbers(number) {
+    for (let i = 0; i < number.length; i++) {
+        digitValue(number[i]);
+    }
+}
+let number="123456";
+printNumbers(number);
